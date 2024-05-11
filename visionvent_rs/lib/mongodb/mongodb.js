bfdf09c07ea8d7c3e956678365c1f349dea8e6f5
Koushik <koushik.mbp@gmail.com>
2024-05-12 00:38:16 +0530

    backend

--- visionvent_rs/lib/mongodb/mongodb.js
+++ visionvent_rs/lib/mongodb/mongodb.js
@@ -3,7 +3,7 @@ import { MongoClient } from "mongodb";
 const connectToDb = async() => {
     try {
         await mongoose.connect(process.env.MONGODB_URI)
-        console.log("Connected to mongoDB")
+        // console.log("Connected to mongoDB")
     } catch (err) {
         console.log(err)
     }
