f0e06f2fa2b96e8f4d355cb5b6521138e3eee7e9
Koushik <koushik.mbp@gmail.com>
2024-05-12 03:31:47 +0530

    changed routes

--- visionvent_rs/app/api/createResearchCenter/route.js
+++ visionvent_rs/app/api/createResearchCenter/route.js
@@ -9,7 +9,7 @@ export async function POST(request) {
             return NextResponse.json({
                 message: "Authentication required!!!"
             }, {
-                status: 401
+                status: 400
             })
         }
     }
@@ -26,13 +26,13 @@ export async function POST(request) {
             message: "Research Center created successfully",
             result: result
         }, {
-            status: 201
+            status: 200
         })
     } catch (error) {
         return NextResponse.json({
             error: error.message
         }, {
-            status: 400
+            status: 500
         })
     }
 }
