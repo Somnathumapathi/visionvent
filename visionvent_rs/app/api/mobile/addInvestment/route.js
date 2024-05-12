ee3bd900002014fbe4031a26bc8f56d71c2253d5
Koushik <koushik.mbp@gmail.com>
2024-05-12 10:01:48 +0530

    changing return

--- visionvent_rs/app/api/mobile/addInvestment/route.js
+++ visionvent_rs/app/api/mobile/addInvestment/route.js
@@ -57,7 +57,7 @@ export async function POST(request) {
 
         return NextResponse.json({
             investorInvestments: investorInvestmentsList,
-            totalInvestment: total,
+            walletBalance: wallet,
         }, {
             status: 200
         })
