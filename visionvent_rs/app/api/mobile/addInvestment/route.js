427fed88546860081c2eaeb40e5c578c12f586f9
Koushik <koushik.mbp@gmail.com>
2024-05-12 09:51:37 +0530

    solving error

--- visionvent_rs/app/api/mobile/addInvestment/route.js
+++ visionvent_rs/app/api/mobile/addInvestment/route.js
@@ -40,7 +40,7 @@ export async function POST(request) {
         // let total = 0
         const total = totalI + body.investedAmount
 
-        const wallet = investor.walletAmt
+        let wallet = investor.walletAmt
         wallet = wallet - body.investedAmount
 
         // console.log(temp.walletAmt)
